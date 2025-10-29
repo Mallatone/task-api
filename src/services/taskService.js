dea2d5f26b6141037b8efe7f221b1a082dc2cfab
Aleck <ascantle@charlotte.edu>
2025-10-28 22:53:41 -0400

    Add Api documentation

--- src/services/taskService.js
+++ src/services/taskService.js
@@ -1,6 +1,7 @@
 import * as taskRepository from '../repositories/taskRepo.js';
 
 export async function getAllTasks() {
+  console.log(taskRepository.findAll());
   return taskRepository.findAll();
 }
 
